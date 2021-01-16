5942388f32ff026bd32f6c56be9cbfccd625ef9d
Shu Teiei <shu_teiei@outlook.jp>
2021-01-16 18:55:19 +0900

    Initial Commit

---
+++ Nafuda/Nafuda.h
@@ -0,0 +1,18 @@
+//
+//  Nafuda.h
+//  Nafuda
+//
+//  Created by 周廷叡 on 2021/01/16.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for Nafuda.
+FOUNDATION_EXPORT double NafudaVersionNumber;
+
+//! Project version string for Nafuda.
+FOUNDATION_EXPORT const unsigned char NafudaVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Nafuda/PublicHeader.h>
+
+
